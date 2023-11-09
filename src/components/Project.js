01461853540cb8aa3721ed4cbf5cd43216b47735
Jean-Charles <108011439+JCharles28@users.noreply.github.com>
2023-11-09 22:15:21 +0100

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,6 @@ export default function Project() {
     return (
       <div className="container">
         <h1>Projet</h1>
-       
-        
         <p className="description-project">
               Notre système d'intelligence artificielle révolutionnaire pour la sécurité routière sera conçu pour détecter et prévenir les comportements dangereux au volant.
               Grâce à des capteurs, notre IA surveille attentivement les actions du conducteur, telles que l'utilisation du téléphone, la consommation de cigarettes ou les signes de somnolence.
@@ -13,7 +11,6 @@ export default function Project() {
               Avec notre solution innovante, nous visons à améliorer la sécurité sur les routes et à réduire les accidents liés à des distractions ou à la fatigue.
               Faites confiance à <span className="sf">SaferRoad</span> pour vous aider à conduire en toute sécurité !
         </p>          
-
       </div>
   );
 }
